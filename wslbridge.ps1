84a1e085b6db9d53fdec3a39dd9a974d79cfc860
Patrick E Imoh <patrickimoh@gmail.com>
2020-07-16 00:40:13 +0100

    add wslbridge.ps1

---
+++ wslbridge.ps1
@@ -0,0 +1,37 @@
+# If Debian and ifconfig isn't supported, uncomment the first line and comment the second	
+$remoteport = bash.exe -c "ip addr | grep -Ee 'inet.*eth0'"
+# $remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
+
+$found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+
+if( $found ){
+  $remoteport = $matches[0];
+} else{
+  Write-Output "The Script Exited, the ip address of WSL 2 cannot be found";
+  exit;
+}
+
+#[Ports]
+
+#All the ports you want to forward separated by coma
+$ports=@(80,443,10000,3000,3390,5000);
+
+
+#[Static ip]
+#You can change the addr to your ip config to listen to a specific address
+$addr='0.0.0.0';
+$ports_a = $ports -join ",";
+
+
+#Remove Firewall Exception Rules
+Invoke-Expression "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
+
+#adding Exception Rules for inbound and outbound Rules
+Invoke-Expression "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Protocol TCP -Action Allow";
+Invoke-Expression "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Protocol TCP -Action Allow";
+
+for( $i = 0; $i -lt $ports.length; $i++ ){
+  $port = $ports[$i];
+  Invoke-Expression "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
+  Invoke-Expression "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+}
